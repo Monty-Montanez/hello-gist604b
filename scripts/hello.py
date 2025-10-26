8fbcf2db293e69d5615296ed00b2c8239c5c2454
Monty Montanez <104440715+Monty-Montanez@users.noreply.github.com>
2025-10-26 07:35:23 -0400

    Update hello.py

--- scripts/hello.py
+++ scripts/hello.py
@@ -3,20 +3,20 @@
 GIST 604B - Open Source GIS
 Module 1 Assignment 3: GitHub Repository Management
 
-Student: [YOUR NAME HERE]
-Date: [TODAY'S DATE]
+Student: Monty Montanez
+Date: 10/26/2025
 University of Arizona
 """
 
 def main():
     """Simple hello world function for GIST 604B"""
     print("Hello from GIST 604B!")
-    print(f"Student: [YOUR NAME HERE]")
+    print(f"Student: Monty Montanez")
     print("Learning open source GIS workflows with GitHub")
     print("University of Arizona coordinates: 32.2319° N, 110.9501° W")
     
     # Add your own message here!
-    print("[ADD YOUR OWN MESSAGE ABOUT WHY YOU'RE INTERESTED IN GIS]")
+    print("I want to use GIS to further my academic and professional careers")
 
 if __name__ == "__main__":
     main()
